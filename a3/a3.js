209d20b71d4bd27376de5b032fd4d3c8df5f7ed3
Cole Krajewski <71843215+krajec53@users.noreply.github.com>
2021-02-25 10:14:35 -0600

    Update a3.js

--- a3/a3.js
+++ a3/a3.js
@@ -40,21 +40,38 @@ var fillIn = function(n1, n2)
 {
 
     /* to be completed */
+    if(fp.isEq(n1,n2))
+        return fp.makeList(n1);
+    else if(fp.isLT(n1,n2))
+        return fp.cons(n1, fillIn( fp.add(n1 , 1) , n2 ) );
+    else if(fp.isLT(n2,n1))
+        return fp.cons(n1, fillIn( fp.sub(n1 , 1) , n2 ) );
     
 };
 
 var removeLast = function(tree)
 {
-
     /* to be completed */
-    
+    if( fp.isNull(tree) )
+        return [ ];
+    else if( fp.isNull( fp.tl(tree) ) )
+        return [ ];
+    else if( fp.isList( fp.hd(tree) ) )
+        return fp.cons( removeLast( fp.hd(tree) ), removeLast( fp.tl(tree) ) );
+    else
+        return fp.cons( fp.hd(tree), removeLast( fp.tl(tree) ) );
 };
 
 var intersperse = function(ms,ns)
 {
 
     /* to be completed */
-    
+    if( fp.isNull(ms) )
+        return ns;
+    else if( fp.isNull(ns) )
+        return ms;
+    else
+        return fp.cons( fp.hd(ms), intersperse( ns, fp.tl(ms) ) );
 };
 
 exports.consecutiveSum = consecutiveSum;
